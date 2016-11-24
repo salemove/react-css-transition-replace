9fdbc1d490c553b03c63a6a9f420fbf4a65cd941
Siim Halapuu <siim.halapuu@gmail.com>
2016-11-24 14:58:54 +0200

    Remove element at once when transitonLeave is false
    Previously transitionLeave being false did not affect the behavior and
    in dom the old component still existed together with the new component.
    
    This does not show old component at all when new component is ready to
    be rendered.

--- src/ReactCSSTransitionReplace.jsx
+++ src/ReactCSSTransitionReplace.jsx
@@ -133,7 +133,7 @@ export default class ReactCSSTransitionReplace extends React.Component {
     if (!this.isTransitioning && !this.state.isLeaving) {
       const { currentChild, nextChild } = this.state;
 
-      if (currentChild && (nextChild || nextChild === false || nextChild === null)) {
+      if (currentChild && (nextChild || nextChild === false || nextChild === null) && this.props.transitionLeave) {
         this.leaveCurrent();
       }
       if (nextChild) {
@@ -261,7 +261,7 @@ export default class ReactCSSTransitionReplace extends React.Component {
             transitionAppearTimeout, transitionEnterTimeout, transitionLeaveTimeout,
             ...containerProps } = this.props;
 
-    if (currentChild) {
+    if (currentChild && !nextChild && !transitionLeave || currentChild && transitionLeave) {
       childrenToRender.push(
         React.createElement(
           'span',
